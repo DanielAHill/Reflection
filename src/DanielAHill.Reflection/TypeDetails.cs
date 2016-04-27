5c996d047c957a1495c73a5094af0b57077a4379
Daniel Hill <git@DanielAHill.com>
2016-04-26 21:33:43 -0600

    Include more types as value types

--- src/DanielAHill.Reflection/TypeDetails.cs
+++ src/DanielAHill.Reflection/TypeDetails.cs
@@ -24,7 +24,7 @@ namespace DanielAHill.Reflection
     internal class TypeDetails : ITypeDetails
     {
         public Type Type { get; }
-        public bool IsValue => ValueTypes.Contains(Type) || PropertyReaders.Count + PropertyWriters.Count == 0;
+        public bool IsValue { get; }
         public object DefaultValue { get; }
         public bool IsNumeric => NumericTypes.Contains(Type);
         public bool IsCollection { get; }
@@ -33,18 +33,25 @@ namespace DanielAHill.Reflection
 
         internal TypeDetails(Type type, object defaultValue, IReadOnlyList<IPropertyReader> propertyReaders, IReadOnlyList<IPropertyWriter> propertyWriters)
         {
+            var typeInfo = type.GetTypeInfo();
+
             Type = type;
             DefaultValue = defaultValue;
             PropertyReaders = propertyReaders;  
             PropertyWriters = propertyWriters;
-            IsCollection = type != typeof (string) && typeof (IEnumerable).GetTypeInfo().IsAssignableFrom(type.GetTypeInfo());
+            IsCollection = type != typeof (string) && typeof (IEnumerable).GetTypeInfo().IsAssignableFrom(typeInfo);
+            IsValue = typeInfo.IsValueType
+                      || ValueTypes.Contains(Type)
+                      || PropertyReaders.Count + PropertyWriters.Count == 0
+                      || (typeInfo.IsGenericType && typeInfo.GetGenericTypeDefinition() == typeof(Nullable<>) &&
+                            (type.GetGenericArguments()[0].GetTypeInfo().IsValueType
+                            || ValueTypes.Contains(type.GetGenericArguments()[0])));
         }
 
         private static readonly HashSet<Type> ValueTypes = new HashSet<Type>
         {
             typeof (string),
-            typeof (Guid),
-            typeof (Guid?)
+            typeof (Guid)
         };
 
         private static readonly HashSet<Type> NumericTypes = new HashSet<Type>
